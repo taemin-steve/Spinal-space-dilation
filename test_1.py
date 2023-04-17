322c788be7d86e6803c2e0611f4a4a44945972aa
minkyokyo <71928522+minkyokyo@users.noreply.github.com>
2023-04-18 02:06:31 +0900

    reprojection test code ( ten images)

--- test_1.py
+++ test_1.py
@@ -2,12 +2,9 @@ import cv2 as cv
 import numpy as np
 import math
 from circle import make_2D_points
+import glob
 
-IMG_PATH ='./circle_images/1_Color.png'
-PATTERN_SIZE = (6,3) # 18 circle exist
-UNIT_SIZE = 47.8125 # distance between circles // unit is millimeter
-imgInit = cv.imread(IMG_PATH,cv.IMREAD_GRAYSCALE)
-H, W = imgInit.shape[:2] 
+
 
 # ----------------------------------원 좌표 구하기 위한 코드 --------------------------------------
 
@@ -28,40 +25,54 @@ params.minDistBetweenBlobs = 1
 
 # detector를 만듭니다.
 detector = cv.SimpleBlobDetector_create(params)
+    
+PATTERN_SIZE = (6,3) # 18 circle exist
+UNIT_SIZE = 47.8125 # distance between circles // unit is millimeter
 
-# keyPoints는 detector가 원을 감지한 것
-keyPoints = detector.detect(imgInit)
+for j in range(10):
+    IMG_PATH ='./circle_images/'+ str(j + 1)+ '_Color.png'
+    imgInit = cv.imread(IMG_PATH,cv.IMREAD_GRAYSCALE)
+    H, W = imgInit.shape[:2] 
 
-# 원을 잘 감지했는지 visualiaze 하는 코드입니다.
-for i in range(len(keyPoints)):
-    keypoint = keyPoints[i]
+    # keyPoints는 detector가 원을 감지한 것
+    keyPoints = detector.detect(imgInit)
 
-    x = int(keypoint.pt[0])
-    y = int(keypoint.pt[1])
-    s = keypoint.size
-    r = int(math.floor(s / 2))
-    cv.circle(imgInit, (x, y), r, (256, 200, 0), 3) # imgInit 파일에 원을 그려넣음.
+    # 원을 잘 감지했는지 visualiaze 하는 코드입니다.
+    for i in range(len(keyPoints)):
+        keypoint = keyPoints[i]
 
-cv.imshow("circle",imgInit)
-cv.waitKey(0)
+        x = int(keypoint.pt[0])
+        y = int(keypoint.pt[1])
+        s = keypoint.size
+        r = int(math.floor(s / 2))
+        cv.circle(imgInit, (x, y), r, (256, 200, 0), 3) # imgInit 파일에 원을 그려넣음.
+
+    #cv.imshow(str(j),imgInit)
+    
+    #cv.waitKey(0)
 
 #2D 좌표
 points2Ds =[]
 
-img_gray = cv.imread(IMG_PATH, cv.IMREAD_GRAYSCALE)
-# circle 좌표를 찾음
-ret, corners = cv.findCirclesGrid(imgInit,(6,3),flags=cv.CALIB_CB_SYMMETRIC_GRID+cv.CALIB_CB_CLUSTERING,blobDetector=detector)
-points2Ds.append(corners)
-# flip
-points2Ds[0]=np.flip(points2Ds[0],0)
-
+for i in range(10):
+    IMG_PATH ='./circle_images/'+ str(i + 1)+ '_Color.png'
+    img_gray = cv.imread(IMG_PATH, cv.IMREAD_GRAYSCALE)
+    # circle 좌표를 찾음
+    ret, corners = cv.findCirclesGrid(img_gray,(6,3),flags=cv.CALIB_CB_SYMMETRIC_GRID+cv.CALIB_CB_CLUSTERING,blobDetector=detector)
+    if ret:
+        points2Ds.append(corners)
+        if i == 7:
+            continue # 8번째 사진은 flip 안해두됨!
+        # flip
+        points2Ds[i]=np.flip(points2Ds[i],0)
+    
 # 3D 좌표
 pattern_points = np.zeros((PATTERN_SIZE[0] * PATTERN_SIZE[1], 3), np.float32)
 pattern_points[:, :2] = np.indices(PATTERN_SIZE).T.reshape(-1, 2)
 pattern_points *= UNIT_SIZE
-points3Ds=[]
-points3Ds.append(pattern_points)
+points3Ds = [pattern_points for i in range(10)]
 
+'''
 # -------------------- 원 잘 그려졌는지 판별하는 코드
 fontFace = cv.FONT_HERSHEY_SIMPLEX
 fontScale = 1
@@ -81,6 +92,7 @@ for j in range(18): # 원의 개수만큼 그린당.
 
 cv.imshow("detect",imgColor)
 cv.waitKey(0)
+'''
 
 # ------------ Camera calibrate
 rms_err, mtx, dist, rvecs, tvecs = cv.calibrateCamera(points3Ds, points2Ds, (W, H), None, None)   
@@ -89,8 +101,15 @@ print("camera intrinsic matrix:\n", mtx)      # 카메라 내부 매트릭스
 print("distortion coefficients: ", dist.ravel()) # 왜곡 계수 출력
 
 
-# reprojection output
-img_rePro = cv.imread(IMG_PATH)
+
+fontFace = cv.FONT_HERSHEY_SIMPLEX
+fontScale = 1
+color = (0, 255, 0)
+thickness = 2
+lineType = cv.LINE_AA
+
+# reprojection output - 1번째 사진만.
+img_rePro = cv.imread('./circle_images/1_Color.png')
 mean_error=0
 # 3D 공간의 점을 2d 이미지로 reprojection.
 # imgpoints2는 tuple형식. shape : ( N, 1, 2 ), N은 3d point의 개수. 
