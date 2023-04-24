4277332e72473a204e250ff9c79a6909ef47720b
minkyokyo <71928522+minkyokyo@users.noreply.github.com>
2023-04-24 15:33:17 +0900

    str to float, 내부파라미터 값 입력, point list -> numpy 변환

--- EHmin/get_camera_pos.py
+++ EHmin/get_camera_pos.py
@@ -10,12 +10,14 @@ with open('./sub_info/circle_pos_noneBlank.txt', 'r') as file:
     count = 0
     for line in file:
         count += 1
-        point2D.append([line.strip().split()])
+        lineList = line.strip().split() 
+        lineList=[float(i) for i in lineList] # str to float
+        point2D.append([lineList])
         if count == 7:
             imgp.append(np.array(point2D).astype(np.float32))
             point2D = []
             count = 0
-            
+imgp = np.array(imgp,dtype=np.float32)    
 #------------------------------------------------------------------------------------------------------
 
 # ----------------------------------get 3D points by textfile--------------------------------------
@@ -31,18 +33,32 @@ for i in range(5):
                 flag = True
                 continue
             if(flag and count < 7):
-                point3D.append(line.strip().split())
-                print(point3D)
+                lineList = line.strip().split() 
+                lineList=[float(i) for i in lineList] # str to float
+                point3D.append([lineList])
+                #print(point3D)
                 count += 1 
         objp.append(np.array(point3D).astype(np.float32))
-                
+objp = np.array(objp,dtype=np.float32) 
+
 #------------------------------------------------------------------------------------------------------
 # 카메라 파라미터
-K = np.array([[500, 0, 320], [0, 500, 240], [0, 0, 1]], dtype=np.float32)
+FX = round(616.2203979492188,3)
+FY = round(616.5223388671875,3)
+CX = round(327.8929443359375,3)
+CY = round(236.85763549804688,3)
+
+'''
+K = [ fx, s, cx]
+    [ 0, fy, cy]
+    [ 0,  0, 1 ]
+'''
+
+K = np.array([[FX, 0, CX], [0, FY, CY], [0, 0, 1]], dtype=np.float32)
 dist = np.array([0, 0, 0, 0], dtype=np.float32)
 
 # solvePnP 함수 호출
-retval, rvec, tvec = cv2.solvePnP(objp, imgp, K, dist)
+retval, rvec, tvec = cv2.solvePnP(objp, imgp, K, dist,flags=cv2.SOLVEPNP_ITERATIVE)
 
 # rvec와 tvec 출력
 print("rvec = ", rvec)
