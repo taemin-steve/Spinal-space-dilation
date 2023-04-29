4a72470e4ce318b46e2cbe65988b32bdb372d42b
EHmin <okchrisok@naver.com>
2023-04-29 14:25:04 +0900

    refactoring

--- EHmin/image_preprocess.py
+++ EHmin/image_preprocess.py
@@ -1,24 +1,6 @@
 import cv2 as cv 
 import numpy as np
 
-# --- opening---
-# for j in range(17):
-#     IMG_PATH ='./newData/'+ str(7062 + j + 1)+ '.png'
-#     imgInit = cv.imread(IMG_PATH,cv.IMREAD_GRAYSCALE)
-#     kernel = np.ones((5, 5), np.uint8)
-#     result = cv.morphologyEx(imgInit, cv.MORPH_OPEN, kernel)
-#     # cv.imshow("2", img_gray)
-#     cv.imwrite("./processed/"+ str(j)+'.png', result)
-# ---------------
-
-# --- bulur---
-# for j in range(17):
-#     IMG_PATH ='./newData/'+ str(7062 + j + 1)+ '.png'
-#     imgInit = cv.imread(IMG_PATH,cv.IMREAD_GRAYSCALE)
-#     result = cv.GaussianBlur(imgInit,(5,5),0)
-#     # cv.imshow("2", img_gray)
-#     cv.imwrite("./processed/"+ str(j)+'.png', result)
-# ---------------
 for j in range(17):
     IMG_PATH ='./newData/'+ str(7062 + j + 1)+ '.png'
     imgInit = cv.imread(IMG_PATH,cv.IMREAD_GRAYSCALE)
